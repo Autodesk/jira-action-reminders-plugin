252ca8687ef77741f9f33fb6e4c9c6d436c3e62b
Venkat Prasad <venkat.prasad@autodesk.com>
2017-10-16 11:33:58 +0800

    fixed rest api

--- src/main/java/com/adsk/jira/actionreminders/plugin/api/AdskActionRemindersResource.java
+++ src/main/java/com/adsk/jira/actionreminders/plugin/api/AdskActionRemindersResource.java
@@ -5,7 +5,6 @@
  */
 package com.adsk.jira.actionreminders.plugin.api;
 
-import com.adsk.jira.actionreminders.plugin.model.ActionRemindersRun;
 import com.adsk.jira.actionreminders.plugin.model.MessageBean;
 import com.atlassian.jira.permission.ProjectPermissions;
 import com.atlassian.jira.project.Project;
@@ -13,8 +12,9 @@ import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.security.PermissionManager;
 import com.atlassian.jira.user.ApplicationUser;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -44,45 +44,52 @@ public class AdskActionRemindersResource {
         this.actionRemindersUtil = actionRemindersUtil;
     }
     
-    @POST
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/config")
-    public Response runActionReminder(ActionRemindersRun actionRemindersRun) {
-        LOGGER.debug("running action reminder - "+actionRemindersRun.getId());
+    @Path("/config/id/{confId}")
+    public Response runActionReminder(@PathParam("confId") String confId) {
+        LOGGER.debug("running action reminder config - "+confId);
         
         //message object
         MessageBean messageBean = new MessageBean();
-        
+
         //check required paramaters
-        if( actionRemindersRun.getId() == 0 ) {
-            messageBean.setMessage("[Error] Required mapId number field is missing.");
-            return Response.status(Response.Status.BAD_REQUEST).entity(messageBean).build();
-        }
-        if( actionRemindersRun.isActions() == false && actionRemindersRun.isReminders() == false ) {
-            messageBean.setMessage("[Error] Required either options actions or reminders.");
+        if( confId == null || "".equals(confId) ) {
+            messageBean.setMessage("[Error] Required config id is missing.");
             return Response.status(Response.Status.BAD_REQUEST).entity(messageBean).build();
         }
         
-        ActionRemindersAO actionReminder = aremindersao.getActionReminderById(actionRemindersRun.getId());
+        long configId;
+        try {
+            configId = Long.parseLong(confId);
+        }catch(ClassCastException e) {
+            messageBean.setMessage("[Error] Required config id is NOT number.");
+            return Response.status(Response.Status.BAD_REQUEST).entity(messageBean).build();
+        }
         
-        //check project permissions 
-        ApplicationUser loggedInAppUser = jiraAuthenticationContext.getLoggedInUser();
-        Project project = projectManager.getProjectObjByKey(actionReminder.getProjectKey());
-        if( project != null ) {
-            if( permissionManager.hasPermission(ProjectPermissions.ADMINISTER_PROJECTS, project, loggedInAppUser) == false ) {
-                messageBean.setMessage("[Error] Permission denied. Project access is required.");
+        ActionRemindersAO actionReminder = aremindersao.getActionReminderById(configId);
+        if(actionReminder != null) {            
+            ApplicationUser loggedInAppUser = jiraAuthenticationContext.getLoggedInUser();
+            Project project = projectManager.getProjectObjByKey(actionReminder.getProjectKey());
+            if( project != null ) {
+                if( permissionManager.hasPermission(ProjectPermissions.ADMINISTER_PROJECTS, project, loggedInAppUser) == false ) {
+                    messageBean.setMessage("[Error] Permission denied. Project access is required.");
+                    return Response.status(Response.Status.FORBIDDEN).entity(messageBean).build();
+                }
+            } else {
+                messageBean.setMessage("[Error] Permission denied. Project is invalid.");
                 return Response.status(Response.Status.FORBIDDEN).entity(messageBean).build();
             }
+
+            actionRemindersUtil.process(actionReminder, 
+                    actionRemindersUtil.getRemindersStatus(), actionRemindersUtil.getActionsStatus());
+
+            messageBean.setMessage("successful.");
+            messageBean.setStatus(1);
         } else {
-            messageBean.setMessage("[Error] Permission denied. Project is invalid.");
-            return Response.status(Response.Status.FORBIDDEN).entity(messageBean).build();
+            messageBean.setMessage("[Error] Invalid config id.");
+            return Response.status(Response.Status.BAD_REQUEST).entity(messageBean).build();
         }
-        
-        actionRemindersUtil.process(actionReminder, 
-                actionRemindersRun.isReminders(), actionRemindersRun.isActions());
-        
-        messageBean.setMessage("successful.");
-        messageBean.setStatus(1);
         
         return Response.ok(messageBean).build();
     }
